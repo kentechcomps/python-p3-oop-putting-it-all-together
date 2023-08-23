6101153b6e758ad66e69e7a7a96b86ee33ad93ba
user <mutukukennedy34@gmail.com>
2023-08-23 18:40:09 +0300

    puttinall togehether

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title , page_count , turn_page):
+        self.title = title
+        self.turn_page = turn_page
+        if type(page_count) is int :
+
+          self.page_count = page_count
+        else:
+          print("page_count must be an integer\n")  
+
+    def turn_page(self , value):
+        self. value=  value
+        print("Flipping the page...wow, you read fast")
+        
+
+book1 = Book("And Then There were None" , 272)
+print(book1.title, book1.page_count)
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self , brand , size ):
+        self.brand = brand
+        if isinstance(size, int):
+            self.size = size
+        else:
+         raise ValueError("size must be an integer")
+
+stan_smith = Shoe("Adidas", 9)
+
+
